9f6481e4a8fd8cd8d856ac6f6de164501b1d8c97
kishan2613 <160527834+kishan2613@users.noreply.github.com>
2026-07-22 23:28:31 +0530

    Frontend commit

--- src/services/backendapis.js
+++ src/services/backendapis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // change when deployed
+  baseURL: "https://futuredekho-server.onrender.com", // change when deployed
 });
 
 export default api;
